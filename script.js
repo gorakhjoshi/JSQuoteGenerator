33bcb8a0487dd9b1ebd649cfd0e395fb122bf765
Gorakh Raj Joshi <gorakh626@gmail.com>
2021-08-26 19:43:01 +0545

    dynamic quote displayed

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
-'use strict';
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
 
-// let apiQuotes = [];
+let apiQuotes = [];
 
 function newQuote() {
-  const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
-  console.log(quote);
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  authorText.textContent = quote.author;
+  quoteText.textContent = quote.text;
 }
 
-// async function getQuotes() {
-//   const apiUrl = 'https://type.fit/api/quotes';
-//   try {
-//     const response = await fetch(apiUrl);
-//     apiQuotes = await response.json();
-//     newQuote();
-//   } catch (error) {
-//     console.log('You entered invalid URL');
-//   }
-// }
+async function getQuotes() {
+  const apiUrl = 'https://type.fit/api/quotes';
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch (error) {
+    console.log('You entered invalid URL');
+  }
+}
 
-// getQuotes();
-
-newQuote();
+getQuotes();

51c201d1ddb6dbddd29057d0abf828b000f7dfb7
Gorakh Raj Joshi <gorakh626@gmail.com>
2021-08-26 19:54:45 +0545

    dynamic font size

--- script.js
+++ script.js
@@ -8,7 +8,18 @@ let apiQuotes = [];
 
 function newQuote() {
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-  authorText.textContent = quote.author;
+
+  if (!quote.author) {
+    authorText.textContent = 'unknown';
+  } else {
+    authorText.textContent = quote.author;
+  }
+  if (quote.text.length > 50) {
+    quoteText.classList.add('long-quote');
+  } else {
+    quoteText.classList.remove('long-quote');
+  }
+
   quoteText.textContent = quote.text;
 }
 

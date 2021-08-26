f2b471df26648f25440732ff2fa35b6fb951904b
Gorakh Raj Joshi <gorakh626@gmail.com>
2021-08-26 19:20:51 +0545

    usng local quotes

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
-let apiQuotes = [];
+'use strict';
+
+// let apiQuotes = [];
 
 function newQuote() {
-  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
   console.log(quote);
 }
 
-async function getQuotes() {
-  const apiUrl = 'https://type.fit/api/quotes';
-  try {
-    const response = await fetch(apiUrl);
-    apiQuotes = await response.json();
-    newQuote();
-  } catch (error) {
-    console.log('You entered invalid URL');
-  }
-}
+// async function getQuotes() {
+//   const apiUrl = 'https://type.fit/api/quotes';
+//   try {
+//     const response = await fetch(apiUrl);
+//     apiQuotes = await response.json();
+//     newQuote();
+//   } catch (error) {
+//     console.log('You entered invalid URL');
+//   }
+// }
 
-getQuotes();
+// getQuotes();
+
+newQuote();
